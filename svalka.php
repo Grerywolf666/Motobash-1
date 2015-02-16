a022866f647f261e7111cd517c7b6bb48ab1ac8a
Greywolf <tsarukwolf@gmail.com>
2015-02-16 12:50:19 +0300

    Вместо гаража

---
+++ svalka.php
@@ -0,0 +1,43 @@
+﻿<?php include("header.php");
+$collect=mongodb_connect_bezdna ();
+//$collect_main=mongodb_connect_main ();
+$pagen = Whatpagenumber($collect[collect_bezdna], $_REQUEST['pagen'],"new");
+$status="new";
+
+//echo "Итог по переменной == $pagen";
+?>
+
+
+<main role="main">
+
+
+
+<?php if($pagen!='PageError!')
+{
+	?>
+
+        <!--start PAGINATION BLOCK-->
+      <?php page_count_number($pagen,$collect[collect_bezdna], "new"); ?>
+        <!--end PAGINATION BLOCK-->
+
+
+
+        <!--start QUITE BLOCK-->
+        <?php	$i=0; post_page_bezdna($pagen, $collect, "new", $show_admin); ?>
+        <!--end QUOTE BLOCK-->
+
+        <!--start PAGINATION BLOCK-->
+        <?php page_count_number($pagen, $collect[collect_bezdna], "new"); ?>
+        <!--end PAGINATION BLOCK-->
+<?php }
+	else
+	{
+		echo "<br><br>Здесь еще нет постов. Добавьте еще =)<br>";
+
+	}
+
+	?>
+    </main>
+
+
+<?php include("footer.php");?>
